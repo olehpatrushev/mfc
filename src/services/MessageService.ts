f72531d5787725c2c87bb4e48d1ced6a22757037
Oleh Patrushev <olegpatron1992@gmail.com>
2025-07-02 14:04:26 +0300

    DEV

--- src/services/MessageService.ts
+++ src/services/MessageService.ts
@@ -1,10 +1,26 @@
 import {KeyPressedEvent} from "./keyboard/events/KeyPressedEvent";
+import {KeyboardButton} from "./keyboard/KeyboardButton";
 
 export class MessageService {
+    private message = '';
+
     constructor() {
     }
 
     processKeyPressed(event: KeyPressedEvent) {
-        console.log(event);
+        if (event.getChar()) {
+            this.message += '' + event.getChar();
+        } else if (event.getSpecialChar()) {
+            switch (event.getSpecialChar()) {
+                case KeyboardButton.SPECIAL_CHAR_SPACE:
+                    this.message += ' ';
+                    break;
+                case KeyboardButton.SPECIAL_CHAR_BACKSPACE:
+                    this.message = this.message.slice(0, -1);
+                    break;
+            }
+        }
+
+        console.log(`Message changed: "${this.message}"`)
     }
 }
